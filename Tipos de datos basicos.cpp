3179936aaa5fffba336841bfaf06c15b028a9f91
CesarG03 <78567001+CesarG03@users.noreply.github.com>
2021-02-17 22:47:28 -0600

    Add files via upload

---
+++ Tipos de datos basicos.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+	
+	int entero = 15;
+	float flotante = 10.45;
+	double mayor = 16.4354;
+	char letra = 'a';
+	
+	cout<<letra;
+	
+	cout<<entero;
+	
+	cout<<flotante;
+	
+	cout<<mayor;
+	
+	return 0;
+}
